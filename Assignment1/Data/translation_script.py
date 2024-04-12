58117291d5584b6bb11913ec6677e26563509b52
RafaelSGil <rafaelgil131@gmail.com>
2024-04-12 17:18:16 +0100

    Changed data and translation script to make delete operations easier

--- Assignment1/Data/translation_script.py
+++ Assignment1/Data/translation_script.py
@@ -8,8 +8,8 @@ pred = Namespace("http://ws.org/netflix_info/pred/")
 g = Graph()
 
 # Helper function to generate unique URIs for values
-def generate_uri(value, prefix):
-    return URIRef(n + prefix + '_' + value.replace(' ', '_'))
+def generate_uri(value):
+    return URIRef(n + value.replace(' ', '_'))
 
 with open('netflix_titles.csv', 'r', encoding='utf-8') as csvfile:
     reader = csv.DictReader(csvfile)
@@ -19,14 +19,16 @@ with open('netflix_titles.csv', 'r', encoding='utf-8') as csvfile:
         
         # Add triples for each attribute
         for column in ['type', 'title', 'director', 'country', 'date_added', 'release_year', 'rating', 'duration']:
-            encoded_value = quote(row[column], safe='')
-            attribute_uri = generate_uri(encoded_value, column)
+            encoded_value = row[column].replace(' ', '_')
+            encoded_value = quote(encoded_value, safe='')
+            attribute_uri = generate_uri(encoded_value)
             g.add((show_uri, pred[column], attribute_uri))
             g.add((attribute_uri, pred.real_name, Literal(row[column])))
 
         # Encode description value properly
-        description_value = quote(row['description'], safe='')
-        description_uri = generate_uri(description_value, 'description')
+        description_value = row['description'].replace(' ', '_')
+        description_value = quote(description_value, safe='')
+        description_uri = generate_uri(description_value)
         g.add((show_uri, pred.description, description_uri))
         g.add((description_uri, pred.real_name, Literal(row['description'])))
 
@@ -37,8 +39,9 @@ with open('netflix_titles.csv', 'r', encoding='utf-8') as csvfile:
             for value in values:
                 value = value.strip()
                 if value:
-                    encoded_value = quote(value, safe='')
-                    value_uri = generate_uri(encoded_value, column)
+                    encoded_value = value.replace(' ', '_')
+                    encoded_value = quote(encoded_value, safe='')
+                    value_uri = generate_uri(encoded_value)
                     g.add((show_uri, pred[column], value_uri))
                     g.add((value_uri, pred.real_name, Literal(value)))
 
